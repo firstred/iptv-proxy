544f0a2d50663bfdaeb8d65042dc9d274e21d837
Michael Dekker <git@michaeldekker.nl>
2025-04-24 10:10:00 +0200

    Java cleanup

--- src/main/kotlin/io/github/firstred/iptvproxy/dtos/m3u/M3uDoc.kt
+++ src/main/kotlin/io/github/firstred/iptvproxy/dtos/m3u/M3uDoc.kt
@@ -1,3 +1,3 @@
 package io.github.firstred.iptvproxy.dtos.m3u
 
-data class M3uDoc(@JvmField val channels: List<M3uChannel>, val props: Map<String, String>)
+data class M3uDoc(val channels: List<M3uChannel>, val props: Map<String, String>)
