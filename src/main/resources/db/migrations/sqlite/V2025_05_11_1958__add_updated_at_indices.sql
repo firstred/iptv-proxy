877b701f7b1356d8995ee9978c3daf4b28f8b70e
Michael Dekker <git@michaeldekker.nl>
2025-05-11 20:02:04 +0200

    Add updated_at column indices

---
+++ src/main/resources/db/migrations/sqlite/V2025_05_11_1958__add_updated_at_indices.sql
@@ -0,0 +1,7 @@
+create index idx_25e7cf55a632d841ce4ec97d2fbd5104 on category (updated_at);
+create index idx_3412a2db6d403fdea12c1d77c8f70bb9 on channel (updated_at);
+create index idx_3357733a923d8a3b80444c37f24391c8 on epg_channel (updated_at);
+create index idx_6fb648ed1b28db15388e61b0adfd8107 on epg_programme (updated_at);
+create index idx_d0fffc43016f6bdad89223004eb83891 on live_stream (updated_at);
+create index idx_09fb5f291895324112eda765a6c7d6eb on movie (updated_at);
+create index idx_8217fa49411ce2079f9f9d7ec608a38f on series (updated_at);
