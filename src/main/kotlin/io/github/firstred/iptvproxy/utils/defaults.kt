eb55841b00433d4bad34e72f934a8cef8641418a
Michael Dekker <git@michaeldekker.nl>
2025-12-30 01:15:28 +0100

    Update varchar length

--- src/main/kotlin/io/github/firstred/iptvproxy/utils/defaults.kt
+++ src/main/kotlin/io/github/firstred/iptvproxy/utils/defaults.kt
@@ -2,7 +2,7 @@ package io.github.firstred.iptvproxy.utils
 
 const val defaultMaxConnections = 6u
 const val defaultMaxRetries = 3u
-const val defaultVarcharLength = 255
+const val defaultVarcharLength = 1024
 const val maxServerNameLength = 127
 const val maxCacheableVideoChunkSize = 33554432 // 32MB
 const val databaseExpressionTreeLimit = 990
