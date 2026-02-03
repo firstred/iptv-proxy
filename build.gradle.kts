b04fe9a0192d755ae9978dd7e8d8858dc45f3efd
Michael Dekker <git@michaeldekker.nl>
2026-02-03 22:32:47 +0100

    Set to version 0.5.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ val localProperties = Properties()
 localProperties.load(FileInputStream(rootProject.file("local.properties")))
 
 group = "io.github.firstred"
-version = "0.5.8"
+version = "0.5.9"
 
 application {
     mainClass.set("$group.iptvproxy.ApplicationKt")
